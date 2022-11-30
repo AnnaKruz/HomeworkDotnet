4d922fb5bf177bae962b993db4eb547599ede436
Anna <anutakruz@ya.ru>
2022-11-30 21:06:16 +0500

    Add homework 6

---
+++ Work001/Task-6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ведите первое число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write($"{number} - четное число");
+}
+else 
+{
+    Console.Write($"{number} - нечетное число");
+}
