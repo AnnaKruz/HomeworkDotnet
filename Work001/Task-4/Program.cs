907fd6e096597189aaeb9e1540ddb779bcfe604c
Anna <anutakruz@ya.ru>
2022-11-30 20:54:39 +0500

    Add homework 4

---
+++ Work001/Task-4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Ведите первое число: ");
+int max = int.Parse(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Ведите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3) 
+{
+    max = number3;
+}
+
+Console.Write($"max = {max}");
