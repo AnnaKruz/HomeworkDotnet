a3ed237c03c8d0900585deacb33c27f56937d220
Anna <anutakruz@ya.ru>
2022-11-30 21:21:42 +0500

    Add homework 8

---
+++ Work001/Task-8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Ведите первое число: ");
+int number = int.Parse(Console.ReadLine());
+int result = 2;
+
+while (result <= number)
+{
+    Console.Write($"{result}, ");
+    result += 2;
+}
