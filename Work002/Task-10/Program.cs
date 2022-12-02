df3a56d3c43d57b57d4f92293af90c8b024d260a
Anna <anutakruz@ya.ru>
2022-12-03 01:21:30 +0500

    Add homework Task 10

---
+++ Work002/Task-10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = number / 10 % 10;
+Console.Write($"Вторая цыфра в числе {number} - {result});
+
