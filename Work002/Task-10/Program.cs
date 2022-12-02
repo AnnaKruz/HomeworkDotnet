00bc978deceab9f27d4c0ca612d006f90913283e
AnnaKruz <101725366+AnnaKruz@users.noreply.github.com>
2022-12-03 02:48:58 +0500

    Update Program.cs
    Clear error

--- Work002/Task-10/Program.cs
+++ Work002/Task-10/Program.cs
@@ -1,6 +1,5 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
+Console.WriteLine("Введите трехзначное число: ");
 int number = int.Parse(Console.ReadLine());
 
 int result = number / 10 % 10;
-Console.Write($"Вторая цыфра в числе {number} - {result});
-
+Console.Write($"Вторая цифра в числе {number} - {result});
