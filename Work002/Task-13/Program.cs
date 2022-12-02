d1692c7511fc7e988b23347acce3c4b1a15fcee9
Anna <anutakruz@ya.ru>
2022-12-03 02:44:08 +0500

    Add homework Task 13

---
+++ Work002/Task-13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 99) {
+    while (number > 999) {
+    number = number/10;
+}
+int result = number%10;
+Console.Write($"Третья цифра в вашем числе - {result}");
+}
+else Console.Write("Третьей цифры нет");
+
+
+// while (result > 10) {
+//     result = result/10;
+// }
+// Console.Write($"Третья цыфра в числе {number} - {result}");
