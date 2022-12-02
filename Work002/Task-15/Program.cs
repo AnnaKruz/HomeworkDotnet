769a0092c5208b224722dec1225f31aa5058fb05
Anna <anutakruz@ya.ru>
2022-12-03 03:07:43 +0500

    Add homework Task 15

---
+++ Work002/Task-15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 7 || number == 6) Console.Write("да, это выходной");
+if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5) Console.Write("не является выходным");
+if (number > 7) Console.Write("нет такого дня");
