1f4d2e6d24b18d3252e531940cdcf52a30cab9cd
Anna <anutakruz@ya.ru>
2022-12-27 21:10:50 +0500

    Add homework Task 54

---
+++ Work008/Task-54/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void SortRowsArrayTwo(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            for (int j = 0; j < array.GetLength(1)-k-1; j++)
+            {
+
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int tmpParam = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = tmpParam;
+                }
+
+            }
+        }
+    }
+}
+
+void PrintArrayTwo(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int rows, int colums, int begin, int end)
+{
+    int[,] array = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            array[i, j] = new Random().Next(begin, end + 1);
+        }
+    }
+    return array;
+}
+
+int[,] matrix = FillArray(3, 4, 0, 10);
+// Выводим массив
+PrintArrayTwo(matrix);
+SortRowsArrayTwo(matrix);
+Console.WriteLine();
+PrintArrayTwo(matrix);
